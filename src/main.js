8e8787f1a4f59f94ffbeb286f35ed72aed220691
User <zaalishvililuka66@gmail.com>
2024-11-15 22:55:55 +0400

    assigment 2 is done

--- src/main.js
+++ src/main.js
@@ -1,1 +1,64 @@
-// Work in this directory: src
+//1)
+let lastName = "zaalishvili";
+console.log(lastName);
+
+//2) data type, "5" stringia, 5 numberi
+
+// 3)
+let myAge = 21;
+console.log(myAge);
+
+// 4)
+let isAdult = true;
+console.log(isAdult);
+
+// 5) comparison and logical operators
+
+// 6)
+let greeting = "Hello";
+greeting = greeting.toUpperCase();
+console.log(greeting);
+
+// 7)
+let str = "World";
+str = str.toLowerCase();
+console.log(str);
+
+// 8) before method is called and after method is called value of variable is same Hello.
+
+// 9) undefined
+
+// 10) method is not called in right way, it should be toLowerCase() with capital C.
+
+/* 11) prompt abrunebs strings da stringze   ricxvis damatebisas isini ertmanetis gverdze iqneba/miewebeba
+amitom age unda gadaviyvanot number tipad da shemdeg davumatot ricxvi magalitad:let nextAge = Number(age) + 1;
+*/
+
+// 12)
+let name = prompt("What is your name?");
+let age = Number(prompt("How old are you?"));
+let isInSixties = age > 55 && age < 65; // finish the code so that it prints true if the age is in 60s
+console.log(`${name} is in sixties: ${isInSixties}`);
+
+// 13) value of x is 6.
+
+// 14) value of y is 6.
+
+// 15) x++ is increment operator, it increments the value of x by 1, x-- is decrement operator, it decrements the value of x by 1.
+
+// 16) ++x is increment operator, it increments the value of x by 1 and returns the new value(x),increment happens and then value is returned, x++ is increment operator, it increments the value of x by 1, increment happens after value is used.
+//  17) undefined.
+
+// 18)
+
+let num = 5;
+let num2 = 5;
+console.log("num = num2:", num == num2 ? true : false);
+
+// 19) tu mgzavrebis raodenoba ar aris 30 avtobusi ar gava am shemtxvevashi passengerNum ar udris 30_s da if statementi gaeshveba radgan piroba true aris.
+let passengerNum = 21;
+if (passengerNum != 30) {
+  console.log("avtibusi ar gava");
+}
+
+// 20) stringis da numberis damatebisas number orive shemtxvevashi (number + string, string + number) gadaiqceva stringad da sheertdeba meore stringtan/miewebeba.
